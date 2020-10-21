92ad2d8d1b47dd8629daa3cdcc7243953c6e9d5c
Svitlana Dasgupta <marketexe2016@gmail.com>
2020-10-21 19:28:36 +0300

    fix: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const temp = [];
+    const setOfBr = Object.fromEntries(bracketsConfig);
+
+    for (let letter of str.split('')) {
+        if (temp[0] === letter) {
+            temp.shift();
+        } else if (setOfBr[letter]) {
+            temp.unshift(setOfBr[letter]);
+        } else {
+            return false;
+        }
+    }
+
+    return temp.length === 0;
 }
